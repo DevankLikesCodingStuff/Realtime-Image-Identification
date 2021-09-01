e26472410de1c7f5e78a2c144e6d8faa6ce603c0
DevankLikesCodingStuff <77108500+DevankLikesCodingStuff@users.noreply.github.com>
2021-09-01 20:41:55 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -2,11 +2,32 @@ function setup()
 {
     canvas = createCanvas(300, 300);
     canvas.center();
+    video = createCapture(VIDEO);
+    video.hide();
+    classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/1LTF_-r8V/model.json',modelLoaded);
+//ml5.imageClassifier is used to load the model
+}
+
+function modelLoaded()
+{
+    console.log('Model has Loaded!');
 }
 
 function draw()
 {
-
+image(video, 0, 0,300, 300);
+classifier.classify(video, gotResult);
+//Classifier.classify will do the comparision of live webcam view with the teachable machine model
 }
 
-//https://teachablemachine.withgoogle.com/models/1LTF_-r8V/
+function gotResult(error, results)
+{
+    if (error){
+        console.error(error);
+    }
+    else {
+        console.log(results);
+        document.getElementById("result_object_name").innerHTML = results[0].label;
+        document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3)
+    }
+}
